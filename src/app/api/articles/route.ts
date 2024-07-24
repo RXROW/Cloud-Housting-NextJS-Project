9d97ae0dd1675d7d9fed4823a71a4fb5d9e27a0d
“Eslam“ <aslamalmswdya@gmail.com>
2024-07-24 14:49:53 +0300

    start api

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+const articles =[
+  {
+  userId: 1,
+  id: 1,
+  title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    userId: 2,
+    id: 2,
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      userId: 3,
+      id: 3,
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      },
+      {
+        userId: 4,
+        id: 4,
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+            
+
+
+]
+
+
+
+export function GET(req:NextRequest) {
+
+
+
+  return NextResponse.json(articles , {status:200});
+  
+}
