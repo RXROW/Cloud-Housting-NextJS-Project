d1630f55277fe33240189ffe7f450afac43d10b7
“Eslam“ <aslamalmswdya@gmail.com>
2024-08-04 20:28:49 +0300

    Get Profile and Update

--- src/app/api/users/profile/[id]/route.ts
+++ src/app/api/users/profile/[id]/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/utils/DB";
+import { UpdateUserDTO } from "@/utils/Dtos";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 interface Props {
   params: { id: string };
@@ -21,9 +23,12 @@ export async function DELETE(req: NextRequest, { params }: Props) {
       return NextResponse.json({ message: "User Not Found!" }, { status: 404 });
     }
     const jwtToken = req.cookies.get("JwtToken");
-    const authToken =jwtToken?.value as string;
-  
-    const userFromToken = jwt.verify(authToken,process.env.SECRET_KEY as string) as JwtPayload;
+    const authToken = jwtToken?.value as string;
+
+    const userFromToken = jwt.verify(
+      authToken,
+      process.env.SECRET_KEY as string
+    ) as JwtPayload;
 
     if (userFromToken.id === user.id) {
       await prisma.user.delete({
@@ -40,6 +45,124 @@ export async function DELETE(req: NextRequest, { params }: Props) {
       { message: "Only the user himself can delete his account" },
       { status: 403 }
     );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error!", error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+// ..................
+
+/**
+ * @method GET
+ * @route ~/api/users/profile/:id
+ * @dec  Get Profile By Id
+ * @access Private
+ */
+export async function GET(req: NextRequest, { params }: Props) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(params.id) },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        isAdmin: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    if (!user) {
+      return NextResponse.json({ message: "User Not Found!" }, { status: 404 });
+    }
+    const jwtToken = req.cookies.get("JwtToken");
+    const authToken = jwtToken?.value as string;
+
+    const userFromToken = jwt.verify(
+      authToken,
+      process.env.SECRET_KEY as string
+    ) as JwtPayload;
+
+    if (userFromToken === null || userFromToken.id !== user.id) {
+      return NextResponse.json(
+        { message: "You are not allowed " },
+        { status: 403 }
+      );
+    }
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error!", error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+// ..................
+
+/**
+ * @method PUT
+ * @route ~/api/users/profile/:id
+ * @dec  Update User Profile
+ * @access Private
+ */
+export async function PUT(req: NextRequest, { params }: Props) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: "User Not Found!" }, { status: 404 });
+    }
+
+    const jwtToken = req.cookies.get("JwtToken");
+    const authToken = jwtToken?.value as string;
+
+    let userFromToken;
+    try {
+      userFromToken = jwt.verify(
+        authToken,
+        process.env.SECRET_KEY as string
+      ) as jwt.JwtPayload;
+    } catch (error) {
+      return NextResponse.json({ message: "Invalid Token!" }, { status: 401 });
+    }
+
+    if (!userFromToken || userFromToken.id !== user.id) {
+      return NextResponse.json(
+        { message: "You are not allowed" },
+        { status: 403 }
+      );
+    }
+
+    const body = (await req.json()) as UpdateUserDTO;
+
+    if (body.password) {
+      const salt = await bcrypt.genSalt(10);
+      body.password = await bcrypt.hash(body.password, salt);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: parseInt(params.id) },
+      data: {
+        username: body.username,
+        email: body.email,
+        password: body.password,
+      },
+      select:{
+        id: true,
+        email: true,
+        username: true,
+        isAdmin: true,
+        createdAt: true,
+        updatedAt: true,
+      }
+    });
+
+    return NextResponse.json(updatedUser, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { message: "Internal Server Error!", error: (error as Error).message },
--- src/utils/Dtos.ts
+++ src/utils/Dtos.ts
@@ -1,23 +1,26 @@
 export interface createArticleDTO {
-title:string;
-description:string;
-};
+  title: string;
+  description: string;
+}
 
 export interface updateArticleDTO {
-  title?:string;
-  description?:string;
-  };
-  
-export interface registerUserDTO {
-  username:string;
-  email:string;
-  password:string;
- 
-  };
+  title?: string;
+  description?: string;
+}
 
-  export interface loginUserDTO {
- 
-    email:string;
-    password:string;
-   
-    };
+export interface registerUserDTO {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface loginUserDTO {
+  email: string;
+  password: string;
+}
+
+export interface UpdateUserDTO {
+  username?: string;
+  email?: string;
+  password?: string;
+}
