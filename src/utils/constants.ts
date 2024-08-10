bde5f67b32c67deeaf8242796c2636ce776faff3
“Eslam“ <aslamalmswdya@gmail.com>
2024-08-10 23:39:45 +0300

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
-export const ARTICLE_PER_PAGE =6
-const PRODUCTION_DOMAIN="https://cloud-housting-next-4ul5kepth-eslams-projects-b3899e24.vercel.app/"
-const DEVELOPMENT_DOMAIN="http://localhost:3000"
+export const ARTICLE_PER_PAGE = 6;
+
+const PRODUCTION_DOMAIN = "https://cloud-housting-next-4ul5kepth-eslams-projects-b3899e24.vercel.app/";
+const DEVELOPMENT_DOMAIN = "http://localhost:3000";
+
+// Set DOMAIN based on the environment
 export const DOMAIN = process.env.NODE_ENV === 'production' 
     ? PRODUCTION_DOMAIN
     : DEVELOPMENT_DOMAIN;
