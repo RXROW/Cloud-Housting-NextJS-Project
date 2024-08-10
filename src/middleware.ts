fc24e27f4c3d14642ab2170bddb6ac723f151553
“Eslam“ <aslamalmswdya@gmail.com>
2024-08-11 00:42:00 +0300

    fix cros

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,35 @@
- 
-
-
- 
- 
-//////////////////////////////////////////
-
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function middleware(req: NextRequest) {
+    console.log(req.nextUrl);
 
-    const jwtToken = request.cookies.get("JwtToken");
+    // Handle CORS
+    const res = NextResponse.next();
+    const allowedOrigins = [
+        'https://cloud-housting-next-4ul5kepth-eslams-projects-b3899e24.vercel.app',
+        'http://localhost:3000'
+    ];
+    
+    const origin = req.headers.get('Origin') as string;
+
+    if (allowedOrigins.includes(origin)) {
+        res.headers.append('Access-Control-Allow-Origin', origin);
+    } else {
+        res.headers.append('Access-Control-Allow-Origin', 'null');
+    }
+
+    res.headers.append('Access-Control-Allow-Credentials', 'true');
+    res.headers.append('Access-Control-Allow-Methods', 'GET, DELETE, PATCH, POST, PUT');
+    res.headers.append('Access-Control-Allow-Headers', 
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+
+    // Handle JWT token and routing logic
+    const jwtToken = req.cookies.get("JwtToken");
     const token = jwtToken?.value as string;
 
     if (!token) {
-        if (request.nextUrl.pathname.startsWith("/api/users/profile/")) {
+        if (req.nextUrl.pathname.startsWith("/api/users/profile/")) {
             return NextResponse.json(
                 { message: 'no token provided, access denied' },
                 { status: 401 } // Unauthorized
@@ -21,12 +37,14 @@ export function middleware(request: NextRequest) {
         }
     } else {
         if (
-            request.nextUrl.pathname === "/login" ||
-            request.nextUrl.pathname === "/register"
+            req.nextUrl.pathname === "/login" ||
+            req.nextUrl.pathname === "/register"
         ) {
-            return NextResponse.redirect(new URL("/", request.url));
+            return NextResponse.redirect(new URL("/", req.url));
         }
     }
+
+    return res; // Return the modified response
 }
 
 export const config = {
