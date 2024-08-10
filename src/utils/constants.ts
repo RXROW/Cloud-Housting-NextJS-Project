764aa29cea8921959765ee643c4e2679b55f6a0e
“Eslam“ <aslamalmswdya@gmail.com>
2024-08-10 23:27:03 +0300

    fix domains

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
 export const ARTICLE_PER_PAGE =6
-export const DOMAIN ="https://cloud-housting-next-4ul5kepth-eslams-projects-b3899e24.vercel.app/"
+const PRODUCTION_DOMAIN="https://cloud-housting-next-4ul5kepth-eslams-projects-b3899e24.vercel.app/"
+const DEVELOPMENT_DOMAIN="http://localhost:3000"
+export const DOMAIN = process.env.NODE_ENV === 'production' 
+    ? PRODUCTION_DOMAIN
+    : DEVELOPMENT_DOMAIN;
